a871fa13c8da2a4aa275b109eb30842ae95a255e
Dependra Thapa <119467358+dependrathapa@users.noreply.github.com>
2023-01-25 09:24:28 +0545

    Add files via upload

---
+++ WEEK-9/script.js
@@ -0,0 +1,13 @@
+alert("Donot talk");
+console.log("hello from africa");
+console.log(document.getElementById("paragraph").innerHTML);
+document.getElementById("paragraph").innerHTML= "<h1>hello world</h1>";
+// document.getElementById("paragraph").style.color="purple";
+document.getElementById("paragraph").style.cssText="color:purple; background:yellow";
+var age = 10;
+const age1 =10;
+let age3 = 10;
+
+console.log(age);
+console.log(age1);
+console.log(age3);
