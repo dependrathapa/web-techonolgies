8840b455cac9a6f6a608f227778f02075a323cef
dependra thapa <np03cs4a220246@heraldcollege.edu.np>
2023-02-05 22:37:05 +0545

    My personal portfolio

---
+++ week 10/script.js
@@ -0,0 +1,12 @@
+// document.getElementById("p").innerHTML="Hey this is week10";
+// document.getElementById("p").style.backgroundColor="pink";
+// document.getElementsByClassName("p1")[0].innerHTML="hello K xa Thikaii cha";
+// document.getElementsByClassName("p1")[0].style.backgroundColor="purple";
+document.getElementsByTagName("h1")[1].
+innerHTML="getElementsByTagname";
+var select = document.getElementsByClassName("p1");
+for(var i=0; i<=select.length; i++){
+    select[i].innerHTML="bro Whats up??";
+    select[i].style.cssText="background-color:plum; color:red font-size:30px";
+    
+}
